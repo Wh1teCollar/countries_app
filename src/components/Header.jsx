3734065550a3383b7c24f41db1a4b1594af9ac15
Mikhail Glushchenko <50820491+Wh1teCollar@users.noreply.github.com>
2022-01-02 20:36:42 +0200

    Home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,7 +46,7 @@ export const Header = () => {
     <HeaderEl>
       <Container>
         <Wrapper>
-          <Title>Where is the world?</Title>
+          <Title>Countries of the world</Title>
           <ModeSwitcher onClick={toggleTheme}>
             {theme === 'light' ? (
               <IoMoonOutline size="14px" />
